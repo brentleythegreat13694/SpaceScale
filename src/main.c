8b2cd747b79202a97081bf4a7ee223785ac2bf43
brentleythegreat13694 <bozemanbrentley659@gmail.com>
2025-12-23 02:19:11 +0000

    Commit - Added Main code that Calculates your weight if you were on a another planet

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    //Contains variables to store weight and planet section
+    double EarthWeight;
+    int choice;
+    double total;
+    //Prompts User for their weight on earth and there choice
+    printf("What is your weight on earth: ");
+    scanf("%lf", &EarthWeight);
+    printf("1. Mercury, 2. Venus, 3. Mars: ");
+    scanf("%d", &choice);
+
+    // Calculate weight depending on what planet you chosen
+    switch (choice) {
+        case 1:
+               total = EarthWeight * 0.38;
+               break;
+        case 2:
+               total = EarthWeight * 0.91;
+               break;
+        case 3:
+               total = EarthWeight * 0.38;
+               break;
+        default:
+              printf("Invalid choice!\n");
+              return 1;
+    }
+    //Tells user their new weight
+    printf("Your new weight would be %.2lf\n", total);
+
+    return 0;
+}
