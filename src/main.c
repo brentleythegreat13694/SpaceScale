69e1a56828d15f3c793ba395df3d40b720507cc9
brentleythegreat13694 <bozemanbrentley659@gmail.com>
2025-12-26 12:46:50 -0600

    Commit - Made Code more secure and also added more error handling

--- src/main.c
+++ src/main.c
@@ -1,30 +1,47 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+void clearInputBuffer(){
+              int c;
+              while ((c = getchar()) != '\n' && c != EOF);
+}
 
 int main(){
     //Contains variables to store weight and planet section
-    double EarthWeight;
+    double earthWeight;
     int choice;
     double total;
-    //Prompts User for their weight on earth and there choice
+    //Prompts User for their weight on earth and their choice and confirms validation
     printf("What is your weight on earth: ");
-    scanf("%lf", &EarthWeight);
-    printf("1. Mercury, 2. Venus, 3. Mars: ");
-    scanf("%d", &choice);
-
-    // Calculate weight depending on what planet you chosen
-    if (EarthWeight == 0){
-        printf("Sorry can not give new weight since your EarthWeight is set to 0");
+    if (scanf("%lf", &earthWeight) != 1){
+        printf("Error: Invalid input. Please enter a number.\n");
+        clearInputBuffer();
         return 1;
     }
+    clearInputBuffer();
+
+    if (earthWeight <= 0){
+        printf("Error: Weight must be greater than 0");
+        return 1;
+    }
+    printf("1. Mercury, 2. Venus, 3. Mars: ");
+    if (scanf("%d", &choice) != 1){
+        printf("Error: Invaild input. Please enter a number (1-3).\n");
+        clearInputBuffer();
+        return 1;
+    }
+    clearInputBuffer();
+
+    // Calculate weight depending on what planet you chosen
     switch (choice) {
         case 1:
-               total = EarthWeight * 0.38;
+               total = earthWeight * 0.38;
                break;
         case 2:
-               total = EarthWeight * 0.91;
+               total = earthWeight * 0.91;
                break;
         case 3:
-               total = EarthWeight * 0.38;
+               total = earthWeight * 0.38;
                break;
         default:
               printf("Invalid choice!\n");
